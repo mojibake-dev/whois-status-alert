7e24664aa98f8a354dc518b97a47504e10bd931d
mojibake-dev <m0jibake@pm.me>
2025-04-17 00:15:28 -0700

    readme authoring and main check for existing status file and added logging to functions

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # whois-status-alert
+
+### The Problem
+
+This project is a script to periodically check a domain's status. 
+
+There is a domain I would like, however it currently has the following bizarre statuses. 
+
+'''
+  Domain Status: DeleteProhibited
+  Domain Status: Hold
+  Domain Status: Locked
+  Domain Status: RegistrantTransferProhibited
+'''
+
+this is because it is locked due to its involvement in multiple romanian court cases. 
+I would like to know when it frees up, but a lot of domain brokers do not truck in .ro domains.
+
+So I build this myself.
+
+### The Solution
+
+TODO: detail. 
+
+TODO: This code will evolve over time to be hosted on AWS and ultimately alert via text
+
+### security concerns
+
+Even though the log of the last time the whois was queried is not public facing and it's highly unlikely to be tampered with, we've opted not to use pickle as quoting from python's docs here: https://docs.python.org/3/library/pickle.html
+
+> Warning
+> The pickle module is not secure. Only unpickle data you trust.
+> It is possible to construct malicious pickle data which will execute arbitrary code during unpickling. Never unpickle data that could have come from an untrusted  source, or that could have been tampered with.
+> Consider signing data with hmac if you need to ensure that it has not been tampered with.
+> Safer serialization formats such as json may be more appropriate if you are processing untrusted data. See Comparison with json.
+
+as we only need to store a simple list, json will suffice. 
--- whoisCheck.py
+++ whoisCheck.py
@@ -1,4 +1,4 @@
-import whois, logging, os, pickle
+import whois, logging, os, json
 from dotenv import load_dotenv
 
 
@@ -7,49 +7,69 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s",
     handlers=[
         logging.FileHandler("app.log"),  # Log to a file
-        logging.StreamHandler(),  # Also log to the console
+        logging.StreamHandler(),  # Also log to the console 
     ],
 )
+
 
 def loadEnv():
     load_dotenv()
     domain = os.getenv("DOMAIN")
+    logging.info(f"Loading domain {domain} from .env")
     return domain
 
+
 def loadLastStatus():
+    with open('status.json') as file:
+        lastStatus = json.load(file)
     return lastStatus
+
 
 def grabStatus(domain):
     status = whois.whois(domain).status
+    logging.info(f"grabbed status: {status} for domain {domain}")
     return status
 
-def saveStatus(status):
-    return
 
-def compareStatus():
-    return check
+def saveStatus(status):
+    with open('status.json', 'w') as file:
+        json.dump(status, file)
+    logging.info(f"Logging status {status}")
+
+
+def compareStatus(status, lastStatus):
+    if status == lastStatus:
+        return True
+    else:
+        return False
+
 
 def main():
     
     domain = loadEnv()
 
     status = grabStatus(domain)
-    #log grabing status 
-    
+    logging
+
     # check for last status 
-    # if no 
-        # log 
-        # report 
-    # if yes 
-        # compare
-        # if status same
-            # log 
-            # quit
-        # if status different
-            # log
-            # report
+    try:
+        lastStatus = loadLastStatus()
+    except FileNotFoundError:
+        logging.warning(f"Existing status not found for: {domain}. Generating first status log.")
+        saveStatus(status)
+        return
+
     
-    return
+    # compare
+    # if status same
+        # log 
+        # store
+        # quit
+    # if status different
+        # log
+        # store new
+        # report
+
 
 if __name__ == "__main__":
     main()
